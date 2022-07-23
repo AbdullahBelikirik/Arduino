f83d95c4859a8775a75be33b001c1439b97c4638
AbdullahBelikirik <101903230+AbdullahBelikirik@users.noreply.github.com>
2022-07-23 03:08:00 +0300

    Create Knight Rider Led.ino

---
+++ Knight Rider Led.ino
@@ -0,0 +1,20 @@
+int leds[] = {2,3,4,5,6,7};
+
+void setup() {
+  for(int i=0; i<6; i++){
+   pinMode(leds[i],OUTPUT); 
+  }
+}
+
+void loop() {
+  for(int i=1; i<6; i++){
+    digitalWrite(leds[i],HIGH);
+    delay(100);
+    digitalWrite(leds[i],LOW);
+  }
+  for(int j=5; j>-1; j--){
+    digitalWrite(leds[j],HIGH);
+    delay(100);
+    digitalWrite(leds[j],LOW);
+  }
+}
