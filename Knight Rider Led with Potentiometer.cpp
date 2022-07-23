4829c108e70e43b96eb1085bdb6fa54ec00c910f
AbdullahBelikirik <101903230+AbdullahBelikirik@users.noreply.github.com>
2022-07-24 02:14:50 +0300

    Create Knight Rider Led with Potentiometer.cpp

---
+++ Knight Rider Led with Potentiometer.cpp
@@ -0,0 +1,23 @@
+int leds[] = {2,3,4,5,6,7};
+#define pot A0
+
+void setup() {
+  for(int i=0; i<6; i++){
+   pinMode(leds[i],OUTPUT); 
+  }
+  Serial.begin(9600);
+}
+
+void loop() {
+  int value = analogRead(pot)/20; 
+  for(int i=1; i<6; i++){
+    digitalWrite(leds[i],HIGH);
+    delay(value);
+    digitalWrite(leds[i],LOW);
+  }
+  for(int j=5; j>-1; j--){
+    digitalWrite(leds[j],HIGH);
+    delay(value);
+    digitalWrite(leds[j],LOW);
+  }
+}
