7781e0ac7ba26a25207bd8ceb37b992f24c7a79e
AbdullahBelikirik <101903230+AbdullahBelikirik@users.noreply.github.com>
2022-07-26 02:43:40 +0300

    Update Analog Read with LDR.ino

--- Analog Read with LDR.ino
+++ Analog Read with LDR.ino
@@ -3,12 +3,10 @@
 
 void setup() {
   pinMode(Led, OUTPUT); 
-  Serial.begin(9600);
 }
 
 void loop() {
   int light = analogRead(LDR); 
-  Serial.println(light); 
   delay(50);
   if (light > 900) { 
     digitalWrite(Led, LOW); 
