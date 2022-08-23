0b6eef329a69f8f96a548209f48ff523f9080580
AbdullahBelikirik <101903230+AbdullahBelikirik@users.noreply.github.com>
2022-08-23 09:04:26 +0300

    Update Knight Rider Led with Potentiometer.ino

--- Knight Rider Led with Potentiometer/Knight Rider Led with Potentiometer.ino
+++ Knight Rider Led with Potentiometer/Knight Rider Led with Potentiometer.ino
@@ -1,21 +1,20 @@
 #define pot A0
-int leds[] = {2,3,4,5,6,7};
+int leds[] = {0,1,2,3,4,5,6,7};
 
 void setup() {
-  for(int i=0; i<6; i++){
+  for(int i=0; i<8; i++){
    pinMode(leds[i],OUTPUT); 
   }
-  Serial.begin(9600);
 }
 
 void loop() {
-  int value = analogRead(pot)/20; 
-  for(int i=1; i<6; i++){
+  int value = analogRead(pot)/10;
+  for(int i=1; i<8; i++){
     digitalWrite(leds[i],HIGH);
     delay(value);
     digitalWrite(leds[i],LOW);
   }
-  for(int j=5; j>-1; j--){
+  for(int j=7; j>-1; j--){
     digitalWrite(leds[j],HIGH);
     delay(value);
     digitalWrite(leds[j],LOW);
